fb541d58d7cec614ab0a98c296fddb65468b619d
sarasotafingerprints <mike@sarasotafingerprints.com>
2023-09-13 11:03:47 -0400

    script to make .env template file

---
+++ env-template.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+fs.readFile('.env', 'utf8', (err, data) => {
+  if (err) {
+    throw err;
+  }
+  let tempData = [];
+  let tempEnv = '';
+  data = data.split('\n');
+  data = data.forEach((element) => {
+    let key = element.split('=')[0];
+    if (key.length != 0) {
+      tempData.push(key.concat(`=#Your ${key} here\n`));
+    }
+  });
+  tempData.forEach((element) => {
+    tempEnv = `${tempEnv}${element}`;
+  });
+
+  fs.writeFile('.env.template', tempEnv, function (err) {
+    if (err) throw err;
+    console.log('.env.template file created successfully');
+  });
+});
